acedaf79b8d4de62078b1986d22c2a3eddf95d90
Luis Tovar <130521074+LTovarAlc@users.noreply.github.com>
2024-02-03 14:12:57 -0600

    no continuare este todo app

--- src/components/taskBar/taskBar.jsx
+++ src/components/taskBar/taskBar.jsx
@@ -5,16 +5,15 @@ const TaskBar = () => {
     return(
         <nav className="taskBar">
             <ul className="taskBar__list">
-                <li className="buttons__list"><img src="../../img/calendar.png" alt="" className="taskbaIcon"/></li>
-                <li className="buttons__list"><img src="../../img/papelera.png" alt="Papelera" className="taskbaIcon" /></li>
-                <li className="buttons__list">
+                <button className="buttons__list"><img src="../../img/calendar.png" alt="" className="taskbaIcon"/></button>
+                <button className="buttons__list"><img src="../../img/papelera.png" alt="Papelera" className="taskbaIcon" /></button>
                 <label className="ui-switch">
                     <input type="checkbox" />
                     <div className="slider">
                         <div className="circle"></div>
                     </div>
                 </label>
-                </li>
+
             </ul>
         </nav>
     )
